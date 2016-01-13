9814b330b11d024d06caf899d6871f1079b5c37a
Julie Fischer <julie.fischer@macbookpro-juliefischer.local>
2016-01-13 16:24:01 -0500

    adding afinedoge directory

---
+++ afinedoge/config.js
@@ -0,0 +1,7 @@
+// Put your own Twitter App keys here. See README.md for more detail.
+module.exports = {
+  consumer_key:         'jSUhmTuzzAx1lcKWgNdvwoEat',
+  consumer_secret:      'OEyIPvVxQozuR2PrcZC03KCumjRq8be3X7HvQdl3UBLkCTFBSc',
+  access_token:         '4797313827-CYxeemA44vLzLzk2tZa8el4botbyDu4ELLMkJjP',
+  access_token_secret:  'PAOuWTxMKnWyyj8XXLp8fdglBWskKqzh5M9C464p9wIHK'
+}
